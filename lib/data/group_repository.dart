ffef4b76172762db50f5b8d1962a3e4b3130afaf
松井英 <241205160@ccmailg.meijo-u.ac.jp>
2026-03-25 05:06:55 +0900

    firestore参照機能追加

--- lib/data/group_repository.dart
+++ lib/data/group_repository.dart
@@ -7,15 +7,15 @@ class GroupRepository {
 
   // グループ作成
   Future<String?> setGroup({
-    required String uid,
-    required String groupName
+    required String id,
+    required String group_name
   }) async {
-    final invitationCode = "123456";  // 一旦適当におく
     final group = _db.collection('groups').doc();
+    final String invitationCode = group.id; // 自動生成されたドキュメントid
 
     // 2.グループ作成
     await group.set({
-      'group_name': groupName,
+      'group_name': group_name,
       'invitation_code': invitationCode,
       'created_at': FieldValue.serverTimestamp(), // Googleサーバーの正確な時間を取得
     });
@@ -23,7 +23,7 @@ class GroupRepository {
     // groups_membershipsに管理者登録
     await _db.collection('groups_memberships').add({
       'group_id': group.id,
-      'user_id': uid,
+      'user_id': id,
       'role': 0,
       'joined_at': FieldValue.serverTimestamp(),
     });
@@ -35,14 +35,14 @@ class GroupRepository {
   // グループ参加
   // 6.グループID存在するかチェック
   Future<void> addGroup({
-    required String uid,
-    required String groupId
+    required String id,
+    required String group_id
   }) async {
-    final group = await _db.collection("groups").doc(groupId).get();
+    final group = await _db.collection("groups").doc(group_id).get();
     if (group.exists) {
       await _db.collection("groups_memberships").add({
         'group_id': group.id,
-        'user_id': uid,
+        'user_id': id,
         'role': 1,
         'joined_at': FieldValue.serverTimestamp(),
       });
@@ -53,40 +53,35 @@ class GroupRepository {
 
   // 管理者に招待
   // 11.メンバーリストの取得
-  Future<List<Map<String, dynamic>>> getMembers(String groupId) async {
+  Future<List<Map<String, dynamic>>> getMembers(String group_id) async {
     final members = await _db // membershipsから指定されたgroup_idと同じものの中身を調べる
       .collection("groups_memberships")
-      .where("group_id", isEqualTo: groupId)
+      .where("group_id", isEqualTo: group_id)
       .get();
     
-    return members.docs.map((doc) => doc.data()).toList();  // map => 1つずつ取り出して形を変える
-    /*
-    [
-      {"user_id": "userA", "role": 0, "joined_at": ...},
-      {"user_id": "userB", "role": 1, "joined_at": ...},
-    ]
-    というようなリストの戻り値となるから，user_idリストを取り出したい場合は
+    List<Map<String, dynamic>> ourMembers = [];
+    for (var doc in members.docs) {
+      final userId = doc.data()['user_id'];
+      final userDoc = await _db.collection('profiles').doc(userId).get(); // prifiles内のuser_idが一致するものの中身を調べる
 
-    final members = await GroupRepository().getMembers("グループID");
-    // 1番目の人のIDを取り出す場合
-      String id = members[0]["user_id"];
-    // 全員のIDを画面に出す場合
-      for (var m in members) {
-        print(m["user_id"]);
+      if (userDoc.exists) {
+        final data = userDoc.data()!;  // 中身が絶対あるprofilesのデータ取得
+        ourMembers.add(data);
       }
-    */
+    }
+    return ourMembers;
   }
 
   // 14.管理者リストに追加
   Future<void> changeRole({
-    required String uid,
-    required String groupId,
-    required String newRole
+    required String id,
+    required String group_id,
+    required int role
   }) async {
     final role = await _db
       .collection("groups_memberships")
-      .where("group_id", isEqualTo: groupId)
-      .where("user_id", isEqualTo: uid)
+      .where("group_id", isEqualTo: group_id)
+      .where("user_id", isEqualTo: id)
       .get();
 
     if (role.docs.isNotEmpty) {
@@ -96,8 +91,29 @@ class GroupRepository {
         .collection("groups_memberships")
         .doc(docId)
         .update({
-          "role": newRole,
+          "role": role,
         });
     }
+  }
+
+
+  // 所属グループ一覧
+  Future<List<Map<String, dynamic>>> getGroups(String user_id) async {
+    final groups = await _db // membershipsから指定されたuidと同じものの中身を調べる
+      .collection("groups_memberships")
+      .where("user_id", isEqualTo: user_id)
+      .get();
+
+    List<Map<String, dynamic>> myGroups = [];
+    for (var doc in groups.docs) {
+      final groupId = doc.data()['group_id']; // groups_membership内に自分のuser_idが存在するgroup_idを取得
+      final groupDoc = await _db.collection('groups').doc(groupId).get(); // groups内のgroup_idが一致するものの中身を調べる
+
+      if (groupDoc.exists) {
+        final data = groupDoc.data()!;  // 中身が絶対あるgroupのデータ取得
+        myGroups.add(data);
+      }
+    }
+    return myGroups;
   }
 }
