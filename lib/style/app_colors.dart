c6ea3fc4c0da6d9eb538b852ea213c0cede0a6f4
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2026-03-27 21:47:43 +0900

    カラーパレットの作成

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color Orange = Color(0xFFFF5C00); // オレンジ
+  static Color textLabel = Colors.black.withValues(alpha: 0.50);
+  static const textSub = Color(0xFF6B7280); // サブテキスト
+  static const textHint = Color(0x66000000);
+}
