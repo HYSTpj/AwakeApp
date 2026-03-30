41d0dfb3c505dda464d35f7c8fa6a7f3a20c8261
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2026-03-31 03:04:06 +0900

    テスト用モデルデータの作成

--- lib/models/event_data.dart
+++ lib/models/event_data.dart
@@ -1,19 +1,64 @@
-// lib/models/event_data.dart
+import 'package:flutter/material.dart';
 
-class EventData {
-  String name;           // イベント名
-  String location;       // 目的地名
-  double lat;           // 緯度
-  double lng;           // 経度
-  DateTime scheduledTime; // 集合日時
-  List<String> selectedMembers; // 選択されたメンバーID
+import 'login_page.dart'; // ログインページのインポート
 
-  EventData({
-    required this.name,
-    required this.location,
-    required this.lat,
-    required this.lng,
-    required this.scheduledTime,
-    this.selectedMembers = const [],
-  });
+// Firebaseを利用するためのパッケージ
+import 'package:firebase_core/firebase_core.dart';
+import '../data/firebase_options.dart';
+
+// Supabaseを利用するためのパッケージ
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  // Flutterを初期化
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Firebaseを初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Supabaseを初期化
+  await Supabase.initialize(
+    url: 'https://nnxfbifnaebzfapgbfkr.supabase.co',  // Project URL
+    anonKey: 'sb_publishable_eldYHgMllFya3mprP7AMXw_SJrK6bkv', // Anon Key
+  );
+  
+  // 画面のUIスタート
+  runApp(const MyApp());
+}
+
+// アプリ全体の設定
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // デザインシステム設定
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // これがアプリテーマ
+        //
+        // "flutter run"でアプリケーションを実行してみると
+        // アプリケーションに紫色のツールバーが表示される
+        // 次に，アプリを終了せずに，以下の"colorScheme"の"seedColor"を"Colors.green"に変更してみる
+        // その後，"hot reload"を実行する（変更を保存するか，Flutter対応IDEの"hot reload"ボタンを押すか，
+        // コマンドラインでアプリを起動した場合は"r"キーを押す）
+        //
+        // カウンターがゼロにリセットされていないことに注意
+        // アプリケーションの状態は，再読み込み中に失われない
+        // 状態をリセットするには，代わりに"hot restart"を使用
+        //
+        // これは値だけでなくコードにも適用できる．ほとんどのコード変更は，"hot reload"だけでテストできる
+
+        // デザインタイプの有効設定
+        useMaterial3:true,
+        //アプリ全体の基本色設定
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      // 最初に表示する画面
+      home: LoginPage()
+    );
+  }
 }
