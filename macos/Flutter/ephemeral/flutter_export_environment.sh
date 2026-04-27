8fbe481d88f0230932557c7e5f21153311780646
松井英 <241205160@ccmailg.meijo-u.ac.jp>
2026-04-27 21:56:03 +0900

    エラー解消

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ayane/Projects/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ayane/Projects/HYST"
+export "FLUTTER_ROOT=/Users/hana/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hana/Downloads/AwakeApp-main"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
