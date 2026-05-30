1456304e285c3f6778c56446bf16b38ab0cb3f00
Ryotaro <ry0000taro.dev@gmail.com>
2026-05-30 10:56:48 +0900

    Update profiles_repository.dart

--- lib/data/profiles_repository.dart
+++ lib/data/profiles_repository.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 
 // profiles ドキュメント
